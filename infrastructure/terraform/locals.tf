734dcccfc8fd0adb7b9d3d9ad431aade3e8b00a4
KodiKrishnan <kodikrishnan2307@gmail.com>
2026-07-26 19:49:40 +0530

    refactor(terraform): remove unused locals

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -8,27 +8,4 @@ locals {
     ManagedBy   = "Terraform"
   }
 
-  naming = {
-
-    vpc            = "${local.resource_prefix}-vpc"
-    alb            = "${local.resource_prefix}-alb"
-    ecs_cluster    = "${local.resource_prefix}-ecs"
-    ecr_repository = "${local.resource_prefix}-backend"
-    rds            = "${local.resource_prefix}-mysql"
-
-    sg_alb = "${local.resource_prefix}-alb-sg"
-    sg_ecs = "${local.resource_prefix}-ecs-sg"
-    sg_rds = "${local.resource_prefix}-rds-sg"
-
-    secrets   = "${local.resource_prefix}-app"
-    log_group = "/ecs/${local.resource_prefix}"
-  }
-
-  ports = {
-    http    = 80
-    https   = 443
-    backend = 8080
-    mysql   = 3306
-  }
-
 }
